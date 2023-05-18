be1bfd15344093713ee1ec58b33322fa8034cbb3
PrakharPandey3 <63103704+PrakharPandey3@users.noreply.github.com>
2023-05-18 20:49:55 +0530

    Update main.py
    updated

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 def t(s):
-    for i in range(len(s)-1):
-        if s[i] == '1': 
-            s[i] = '0'
-            if s[i+1] == '0':
-                s[i+1] = '1'
-            else: 
-                s[i+1] = '0'
-    return s
+for i in range(len(s) - 1):
+    if s[i] == '1':
+      s[i] = '0'
+      if s[i + 1] == '0':
+        s[i + 1] = '1'
+      else:
+        s[i + 1] = '1'
+  return s
+
 
 if __name__ == '__main__':
-    a = list("10100101101010001")
-    print(a)
-    while a != t(a.copy()):
-        a = t(a.copy())
-    print(a)
+  a = list("1000010010101001010101010000000101010101111111110000")
+  print(a)
+  while a != t(a.copy()):
+    a = t(a.copy())
+  print(a)
 
